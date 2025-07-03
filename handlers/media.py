0774642dabe2c9ced0b790bdf67936e85a1c6343
DarkKnife <serserpar801014@gmail.com>
2025-07-04 01:28:44 +0300

    Add language, Readme and Refix

--- handlers/media.py
+++ handlers/media.py
@@ -1,3 +1,5 @@
+import logging
+
 from aiogram import Router, F
 from aiogram.types import Message
 from config import Max_File_Size_MB, Max_Duration_Minutes
@@ -23,10 +25,6 @@ media_route = Router()
 async def media_handler(message: Message):
     file = (message.voice or message.video or message.audio)
 
-    if not file:
-        await message.answer("Не удалось определить файл.")
-        return
-
     if file.file_size > Max_File_Size_MB * 1024 * 1024:
         await message.answer("Файл слишком большой.")
         return
@@ -40,15 +38,24 @@ async def media_handler(message: Message):
         return
 
     await message.answer("Файл получен. Начинаю обработку...")
+    logging.info(f"[{message.from_user.id}] Загружен файл: {file.file_id}")
+
+    await message.answer(f"Примерное время обработки: {file.duration:} сек.")
+    logging.info(f"[{message.from_user.id}] Примерное время обработки: {file.duration:} сек.")
 
     try:
+        logging.info(f"[{message.from_user.id}] Извлечение звука из файла начато")
         local_wav = await convert_file(file, message.bot)
-        text = transcribe(local_wav)
+        logging.info(f"[{message.from_user.id}] Звук извлечён, путь: {local_wav}")
 
+        logging.info(f"[{message.from_user.id}] Начата транскрибация")
+        text = await transcribe(local_wav, message.from_user.id)
+        logging.info(f"[{message.from_user.id}] Транскрибация завершена")
 
         if text.strip():
             await message.answer(text)
         else:
             await message.answer("Не удалось распознать речь.")
     except Exception as e:
+        logging.exception(f"[{message.from_user.id}] Ошибка при обработке файла: {e}")
         await message.answer(str(e))
--- run.py
+++ run.py
@@ -6,6 +6,7 @@ from aiogram.enums import ParseMode
 from config import Token
 from handlers.start import start_route
 from handlers.media import media_route
+from handlers.language import language_router
 
 bot = Bot(token=Token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
@@ -13,6 +14,7 @@ dp = Dispatcher()
 
 async def main():
     dp.include_router(start_route)
+    dp.include_router(language_router)
     dp.include_router(media_route)
     await dp.start_polling(bot)
 
